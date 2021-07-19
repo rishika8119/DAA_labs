863b1521169a5eb48c0f85d487496912f5e8f595
RISHIKA KESHARWANI <61060542+rishika8119@users.noreply.github.com>
2021-07-19 17:56:31 +0530

    Create linearsearch.cpp

---
+++ week0/linearsearch.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ///////
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+ 
+  int n;
+    int flag=0;
+    cin>>n;
+    int a[n];
+    int key;
+    cin>>key;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        c=c+1;
+        if(a[i]==key)
+        {
+            flag=1;
+            break;
+        }
+        
+    }
+    if(flag==1)
+    {
+        cout<<"number "<<key<<" is present and number of comparisons performmed are "<<c<<endl;
+    }
+    else if(flag==0){
+        cout<<"number is not present";
+    }
+}
